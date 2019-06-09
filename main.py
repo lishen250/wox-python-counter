604086c1924074731a59d150fb5cf6042eb2cbcf
lishen <lishen@msu.edu>
2019-06-09 11:40:45 -0400

    添加一点东西

--- main.py
+++ main.py
@@ -1,18 +1,52 @@
 # -*- coding: utf-8 -*-
+from wox import Wox,WoxAPI
+import sqlite3 as sql
 
-from wox import Wox
+class Counter(Wox):
 
-class HelloWorld(Wox):
+    whole = 1
 
+
+    # 暂定支持以下几种 方法
+    # ct creat <title> <defaultPos> <defaultAdd> <addOrSub> <defaultValue>
+    # ct list
+    # ct
+    #
     def query(self, query):
         results = []
+
         results.append({
-            "Title": "Hello World",
+            "Title": "Hello World {} times".format(self.whole),
             "SubTitle": "Query: {}".format(query),
-            "IcoPath":"Images/app.ico",
-            "ContextData": "ctxData"
+            "IcoPath":"Images/add.png",
+            "ContextData": "ctxData",
+            "JsonRPCAction": {},
+            "dontHideAfterAction": True
         })
+        retVar = query.split();
+        if retVar[0].lower() == "add":
+            self.debug("yes!" + self.whole)
+            ret = self.format_field(title='调用几次',subtitle=self.whole)
+            results.append(ret)
+
+            self.whole += 1;
+        #
+        #
+        # results.append(self.format_field(title="总共",subtitle=self.whole))
+
         return results
+
+    @staticmethod
+    def format_field(self,title='',subtitle='',icoPath='smile',contextData='ctxData', JsonRPCAction={}, dontHideAfterAction = True):
+        return {
+            "Title": title,
+            "SubTitle": subtitle,
+            "IcoPath":"Images/" + icoPath + ".png",
+            "ContextData": contextData,
+            "JsonRPCAction": JsonRPCAction,
+            "dontHideAfterAction": dontHideAfterAction
+        }
+
 
     def context_menu(self, data):
         results = []
@@ -24,4 +58,4 @@ class HelloWorld(Wox):
         return results
 
 if __name__ == "__main__":
-    HelloWorld()
+    Counter()
